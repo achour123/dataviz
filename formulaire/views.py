253ff3427400f6311555c5083974c62e3af4e3da
Yahya Mezzi <mezzi.yahya@gmail.com>
2020-09-23 19:43:57 +0100

    Update views.py

--- formulaire/views.py
+++ formulaire/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ParticipantsForm
+import csv
 
 # Create your views here.
 def formul(request):
     if request.method=='POST':
         form = ParticipantsForm(request.POST)
-        if form.is_valid():  
-            nom= form.cleaned_data['nom']          
-            prenom= form.cleaned_data['prenom']          
-            email= form.cleaned_data['email']       
-            print("valid")
+        if form.is_valid():
             form.save()
+            with open("./consult.csv", 'a') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=form.cleaned_data.keys())
+                writer.writerow(form.cleaned_data)
+                return redirect("/")
         else:
             form = ParticipantsForm()
 
